92ada6046a22f87443573744425be7c6c22a78b9
yje <yje@yje-PC>
2018-10-15 11:08:59 +0900

    폴더 구조 추가

---
+++ visual/src/test/java/visual/test/service/SampleTest.java
@@ -0,0 +1,14 @@
+package visual.test.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
