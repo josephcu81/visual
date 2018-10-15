626a033b7d41465448f6d7a20a1d5016bd57a3f3
yje <yje@yje-PC>
2018-10-15 11:01:08 +0900

    로그백 적용 및 pom.xml 정보 수정

--- visual/src/main/java/kr/apfs/sample/controller/SampleController.java
+++ visual/src/main/java/kr/apfs/sample/controller/SampleController.java
@@ -1,6 +1,9 @@
 package kr.apfs.sample.controller;
 
 import kr.apfs.sample.service.SampleService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,13 +14,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping({"/sample"})
 public class SampleController
 {
-
+  
+  private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+  
   @Autowired
   private SampleService sampleService;
 
   @RequestMapping({"/index"})
   public String index(@RequestParam(required=false) String name, ModelMap map)
   {
+	  logger.debug("로그를 호출한다.!!!!!!!!!!!!!!!!!");
+	  logger.debug("로그를 호출한다.!!!!!!!!!!!!!!!!!");
+	  logger.debug("로그를 호출한다.!!!!!!!!!!!!!!!!!");
+	  logger.debug("로그를 호출한다.!!!!!!!!!!!!!!!!!");
+	  logger.debug("로그를 호출한다.!!!!!!!!!!!!!!!!!");
     map.addAttribute("greed", sampleService.getGreeding(name));
     return "sample/index";
   }
--- visual/src/main/java/support/config/AppConfig.java
+++ visual/src/main/java/support/config/AppConfig.java
@@ -7,4 +7,17 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages={"kr.apfs"}, includeFilters={@org.springframework.context.annotation.ComponentScan.Filter(type=org.springframework.context.annotation.FilterType.REGEX, pattern={"kr.apfs.*.*ServiceImple"})})
 public class AppConfig
 {
+	/*@Autowired ConfigurableWebApplicationContext servContext;
+    
+    private void setProfile(String newProfile) {
+        ConfigurableWebApplicationContext rootContext  = (ConfigurableWebApplicationContext) servContext.getParent();
+         
+        rootContext.getEnvironment().setActiveProfiles(newProfile);
+        rootContext.refresh();                
+          
+        // Refreshing Spring-servlet WebApplicationContext
+        servContext.getEnvironment().setActiveProfiles(newProfile);
+        servContext.refresh();
+    }*/
+	
 }
