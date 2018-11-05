4312326c896853ee98b178ec89f7febc90d059eb
yeonju <yeonju@yeonju-PC>
2018-11-06 03:05:52 +0900

    데이터 베이스 설정 추가

--- visual/src/main/java/support/config/DataConfig.java
+++ visual/src/main/java/support/config/DataConfig.java
@@ -1,8 +1,63 @@
 package support.config;
+import java.io.IOException;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.ExecutorType;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 @Configuration
-public class DataConfig
-{
+@MapperScan(basePackages={ "kr.apfs.**.mapper" }, sqlSessionTemplateRef="sqlSessionSimple")
+public class DataConfig{
+	
+	private final String jndiname = "jdbc/orcl";
+	
+	@Bean(name="DefaultDataSource")
+	public DataSource datasource() {
+		JndiDataSourceLookup jndi =  new JndiDataSourceLookup();
+		jndi.setResourceRef(true);
+		DataSource dataSource =  jndi.getDataSource(jndiname);
+		
+		return dataSource;
+	}
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(@Qualifier("DefaultDataSource") DataSource datasource) {
+        return new DataSourceTransactionManager(datasource);
+    }
+    
+    @Bean
+	public SqlSessionFactoryBean sqlSession(@Qualifier("DefaultDataSource") DataSource datasource) throws IOException {
+		SqlSessionFactoryBean sqlSession = new SqlSessionFactoryBean();
+		PathMatchingResourcePatternResolver pmrpr =  new PathMatchingResourcePatternResolver();
+		
+		sqlSession.setDataSource(datasource);
+		sqlSession.setConfigLocation(pmrpr.getResource("classpath:/sqlmap/mapper-config.xml"));
+		//sqlSession.setMapperLocations(pmrpr.getResources("classpath:/sqlmap/mapper/*.xml"));
+		sqlSession.setMapperLocations(pmrpr.getResources("classpath:/sqlmap/mapper/**/*.xml"));
+		//sqlSession.setMapperLocations(pmrpr.getResources("classpath:/sqlmap/mapper/**/**/*.xml"));
+		return sqlSession;
+	}
+    
+    @Primary
+	@Bean(name="sqlSessionSimple")
+	public SqlSessionTemplate sqlSessionSimple(@Qualifier("DefaultDataSource") DataSource dataSource) throws Exception {
+		return new SqlSessionTemplate(sqlSession(dataSource).getObject(), ExecutorType.SIMPLE);
+	}
+    
+    @Bean(name="sqlSessionBatch")
+	public SqlSessionTemplate sqlSessionBatch(@Qualifier("DefaultDataSource") DataSource dataSource) throws Exception {
+		return new SqlSessionTemplate(sqlSession(dataSource).getObject(), ExecutorType.BATCH);
+	}
+	
 }
+
