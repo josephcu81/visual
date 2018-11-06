cad4c56b1342f3a658771ba9d6885c6c411dac06
josephcu81PC <josephcu81@gmail.com>
2018-11-06 21:18:30 +0900

    다시 처리

---
+++ visual/src/main/java/kr/apfs/status/mapper/JoinMapper.java
@@ -0,0 +1,18 @@
+package kr.apfs.status.mapper;        
+import java.util.List;
+import java.util.Map;
+
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+import kr.apfs.status.vo.JoinStatusVo;
+
+@Mapper("JoinMapper")
+public interface JoinMapper {
+	
+	/**
+	 * 시도별 가입자 현황 목록 조회
+	 * @param condition
+	 * @return
+	 */
+	List<JoinStatusVo> selectJoinStatusList(Map<String, Object> condition);
+
+}
